634b5801125731e4c363d744e0fd077d29094333
HappyHina <34612499+ky1004@users.noreply.github.com>
2021-12-17 09:35:20 +0900

    update file

---
+++ Javascript/studyJs/basic/app.js
@@ -0,0 +1,18 @@
+// alert("hi");
+
+// variable을 만들기 위해서는 가장 먼저 const라는 걸 사용한다
+// const는 상수이며 바뀌지 않는 값이다
+const a = 5;
+const b = 2;
+
+const myName = "hina";
+
+// 변수명 작성 스타일 타입에는 2가지가 있음
+// veryLongValueName - camelCase(낙타 등 모양), 자바스크립트 유형
+// very_long_value_name - snake_case(뱀 모양), 파이썬 유형
+// 파이썬은 const필요없음
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello "+myName);
