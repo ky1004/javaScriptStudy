d669b55f754136fcabbda3b2bddec1e5676cae4a
ky1004 <pengukim@naver.com>
2021-12-29 23:56:23 +0900

    function example

---
+++ Javascript/studyJs/functionEx/app.js
@@ -0,0 +1,14 @@
+// function이 없으면
+/*
+console.log("안녕 내 이름은 hina야");
+console.log("안녕 내 이름은 동수야");
+console.log("안녕 내 이름은 민정이야");
+console.log("안녕 내 이름은 프로우야");
+*/
+
+// 중복되는 반복 작업을 최소화 하기 위해 function 사용
+function hello() {
+    console.log("Hello my name is C");
+}
+
+hello();
